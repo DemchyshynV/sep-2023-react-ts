7fd8abf46b6aa547cd6c0da560bbb98f68d55d42
Vitaliy Dechyshyn <demchyshyn.v87@gmail.com>
2024-02-16 20:07:29 +0100

    lesson2

--- src/pages/UserDetailsPage.tsx
+++ src/pages/UserDetailsPage.tsx
@@ -1,4 +1,4 @@
-import {useLocation, useParams} from "react-router-dom";
+import {useParams} from "react-router-dom";
 import {useEffect, useState} from "react";
 
 import {useAppLocation} from "../hooks";
