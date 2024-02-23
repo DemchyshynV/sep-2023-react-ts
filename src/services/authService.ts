56ac8c784f3e7eced5f458280c332d3446af4933
Vitaliy Dechyshyn <demchyshyn.v87@gmail.com>
2024-02-23 19:04:07 +0100

    lesson3

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,8 +1,11 @@
 import {apiService} from "./apiService";
+import {AxiosResponse} from "axios";
 
 const authService = {
     async login(): Promise<void> {
         const {data} = await apiService.post<{ access: string }>('auth/login');
+
+        const auth:Promise<AxiosResponse<{access:string}>> = apiService.post<{ access: string }>('auth/login');
         this.setToken(data.access)
     },
     setToken(token: string): void {
